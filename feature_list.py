1cce97325c9713af7a46ed90dec990e929591a59
apoindex <poindexter.andrew@gmail.com>
2021-02-28 20:49:00 -0600

    Update feature_list.py

--- feature_list.py
+++ feature_list.py
@@ -5,15 +5,15 @@ import bisect
 
 def calculate_rolling_oside(df, window=100):
     '''
-    Calculate ratio of buy vs sell adds over a rolling window
+    Calculate ratio of order side (buy/sell) over a rolling window
     '''
 
     df_temp = df.copy()
     df_temp['buys'] = np.where((df_temp['side'] == 1) & (df_temp.action == 'a'), 1, 0)
     df_temp['sells'] = np.where((df_temp['side'] == 0) & (df_temp.action == 'a'), 1, 0)
 
-    df_temp['rolling_buys'] = df_temp['buys'].rolling(window).sum()
-    df_temp['rolling_sells'] = df_temp['sells'].rolling(window).sum()
+    df_temp['rolling_buys'] = df_temp['buys'].rolling(window, min_periods=1).sum()
+    df_temp['rolling_sells'] = df_temp['sells'].rolling(window, min_periods=1).sum()
     df_temp[f'{window}_period_oside_ratio'] = df_temp['rolling_buys'] / df_temp['rolling_sells']
 
     df[f'feature_oratio{window}'] = df_temp[f'{window}_period_oside_ratio']
@@ -37,21 +37,58 @@ def calculate_qty_ratio(df, levels=1):
 
 def calculate_inside_spread(df):
     '''
-    Calculate book-spread at each order event. Nan until orders are present on both sides
+    Calculate book-spread at each order event.
+    Nan until orders are present on both sides
     '''
 
-    df['feature_inside_spread'] = np.where((df['aq0'] > 0) & (df['bq0'] > 0), df['ap0'] - df['bp0'], np.nan)
+    df['feature_inside_spread'] = np.where((df['aq0'] > 0) & (df['bq0'] > 0), df['ap0'] - df['bp0'], 0)
     return df
 
-#def calculate_add_more_aggressive_px(df):
-#    '''
-#    Calculated when an add order sets a more aggressive px on the inside. New px - old px
-#    '''
-#    df['ask_change'] = df['ap0'].diff()
-#    df['bid_change'] = df['bp0'].diff()
+
+def calculate_px_change(df):
+    '''
+    Calculate change in price between previous price and new price set by order
+    Quantity of previous order must be >0 to account for book being empty at beginning of day
+    '''
+    df['ask_change'] = np.where(df['aq0'].shift(1) > 0, df['ap0'].diff(), 0)
+    df['bid_change'] = np.where(df['bq0'].shift(1) > 0, df['bp0'].diff(), 0)
+    df['feature_px_change'] = np.where(df.side == 1, df['bid_change'], df['ask_change'])
+    return df
+
+
+def calculate_vwap(df):
+    '''
+    Calculate volume weighted average price
+    '''
+    df_temp = df.copy()
+    df_temp['order_px'] = np.where(df_temp['side'] == 1, df_temp['bp0'], df_temp['ap0'])
+    df_temp['order_size'] = np.where(df_temp['side'] == 1, df_temp['bq0'], df_temp['aq0'])
+    df_temp['vwap'] = (df_temp['order_px'] * df_temp['order_size']).cumsum() / df_temp['order_size'].fillna(0).cumsum()
+    df['feature_vwap'] = df_temp['vwap']
+    return df
+
+
+def calculate_midpoint(df):
+    '''
+    Calculate midpoint of bid/ask spread
+    '''
+    df['feature_midpoint'] = np.where((df['aq0'] > 0) & (df['bq0'] > 0), (df['ap0'] + df['bp0']) / 2, np.nan)
+    return df
+
+
+def calculate_price_std(df):
+    '''
+    Calculate ewm of standard deviation of price
+    '''
+    df_temp = df.copy()
+    df_temp['order_px'] = np.where(df_temp['side'] == 1, df_temp['bp0'], df_temp['ap0'])
+    df_temp['opx_ewm_std'] = df_temp['order_px'].ewm(alpha=0.01).std().fillna(0)
+    df['feature_px_std'] = df_temp['opx_ewm_std']
+    return df
+
 
 def index(a, x, lo):
-    'Locate the leftmost value exactly equal to x'
+    '''Locate the leftmost value exactly equal to x'''
     i = bisect.bisect_left(a, x, lo=lo)
     if i != len(a) and a[i] == x:
         return i
@@ -59,7 +96,7 @@ def index(a, x, lo):
 
 
 def find_le(a, x, lo):
-    'Find rightmost value less than or equal to x'
+    '''Find rightmost value less than or equal to x'''
     i = bisect.bisect_right(a, x, lo=lo)
     if i:
         return a[i-1]
@@ -71,7 +108,7 @@ def calculate_price_delta(df, window=100):
     '''
     Forward Looking
     Difference between price on order side now and opposite price at curr_timestamp + window in the future.
-    If curr_timestamp + window is greater than end of day, calculate price_delta to last bid/last of the day
+    If curr_timestamp + window is greater than end of day, calculate price_delta to last bid/ask of the day
     '''
     N = len(df)
     t_vec = df['timestamp'].values
@@ -118,8 +155,8 @@ def calculate_price_delta(df, window=100):
 
 # testing
 if __name__ == '__main__':
-    df = pd.read_csv(f"C:/Users/Andrew/Documents/Python Scripts/order_book/data/output_20190614.csv")
-#    df = df.head(10000)
+    df = pd.read_csv(f"output_20190614.csv")
+
     window = 100
     df = calculate_price_delta(df, window=window)
 
